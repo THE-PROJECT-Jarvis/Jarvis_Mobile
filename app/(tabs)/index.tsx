8ddec61e5efe25b3f0c241162322524b4d4c60ad
robin00007 <robin_20220@aitpune.edu.in>
2025-05-20 01:42:16 +0530

    made basic voice to text feature

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,63 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Button, StyleSheet, View } from "react-native";
+import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import Voice from "@react-native-voice/voice";
 
 export default function HomeScreen() {
+  const [result, setResult] = useState("");
+  const [started, setStarted] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    Voice.onSpeechResults = (e) => {
+      if (e.value) {
+        setResult(e.value[0]);
+      }
+    };
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+      setStarted(false);
+    };
+  }, []);
+
+  const startListening = async () => {
+    try {
+      setStarted(started === undefined ? true : !started);
+      await Voice.start("en-US");
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.homeContainer}>
+      <Button
+        title={started ? "STOP " : "Start speaking "}
+        onPress={
+          !started
+            ? startListening
+            : () => {
+                Voice.stop();
+                setStarted(!started);
+              }
+        }
+      />
+      <Text>{result}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  homeContainer: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#282D2A",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -70,6 +69,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
